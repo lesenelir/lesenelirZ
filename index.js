7c0b3f4e04700c695bd2837723a59d0599b09614
lesenelir <hi@lesenelir.me>
2023-09-13 21:09:18 +0800

    feat: update information

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 module.exports = {
-  user: 'lesenelir',
-  sayHi: function () {
-    console.log('hello', this.user)
+  name: 'lesenelir',
+  chineseName: '周彦',
+  gender: 'male',
+  birthday: new Date('1997/01/22'),
+  email: 'hi@lesenelir.me',
+  github: 'https://github.com/lesenelir',
+  location: {
+    country: 'China',
+    hometown: 'Quzhou Zhejiang'
+  },
+  profile: {
+    homepage: 'https://lesenelir.me',
+    xlog: 'https://lesenelir.xlog.app/',
+    mirror: 'https://mirror.xyz/lesenelir.eth'
+  },
+  introduce() {
+    console.log('A soft boy ~')
+  },
+  goodbye() {
+    console.log('Thanks for downloading my self-introduction package!')
   }
 }
